6dbd8662f9aef622d4041993b7f6f88b54911ac4
Jingming Chen <chenjm8683@gmail.com>
2015-11-22 19:39:08 -0800

    initial commit with Phase 5:contact page

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+AuthDemo::Application.routes.draw do
+  resource :session, only: [:create, :destroy, :new]
+  resource :user, only: [:create, :new, :show] do
+    resource :counter, only: [:update]
+  end
+  resources :pages, only: :show
+end
